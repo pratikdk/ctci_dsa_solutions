69efa20f0b52997abba251ceb230c75a1a9d0fdb
pratikdk <pratik.deoolwadikar@gmail.com>
2020-12-25 19:32:56 +0530

    Day 17

---
+++ trees_graphs/10_is_subtree.py
@@ -0,0 +1,31 @@
+from tree_node import TreeNode
+
+def contains_tree(t1, t2):
+    if t2 == None: return True
+    return is_subtree(t1, t2)
+
+def is_subtree(r1, r2):
+    if r1 == None: return False
+    if r1.value == r2.value and matchtree(r1, r2):
+        return True
+    return is_subtree(r1.left, r2) or is_subtree(r1.right, r2)
+
+def matchtree(r1, r2):
+    if r1 == None and r2 == None: return True
+    elif r1 == None or r2 == None: return False
+    elif r1.value != r2.value: return False
+    else:
+        return matchtree(r1.left, r2.left) and matchtree(r1.right, r2.right)
+
+
+if __name__ == "__main__":
+    n1 = TreeNode(1)
+    n2 = TreeNode(2)
+    n3 = TreeNode(3)
+    n4 = TreeNode(4)
+    n5 = TreeNode(5)
+    n2.left = n1
+    n2.right = n3
+    n3.left = n4
+    n3.right = n5
+    print(matchtree(n3, n3))
